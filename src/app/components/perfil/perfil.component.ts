ce14083a44195daf57663ca75d8df7935088b707
JaumePuig <jaumepuigsoriano@gmail.com>
2026-05-06 12:59:17 +0200

    inicio perfil

---
+++ src/app/components/perfil/perfil.component.ts
@@ -0,0 +1,45 @@
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PerfilService } from './perfil.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-perfil',
+  imports: [CommonModule],
+  templateUrl: './perfil.component.html',
+  styleUrl: './perfil.component.css',
+})
+export class PerfilComponent {
+  private router = inject(Router);
+  constructor(private perfilService: PerfilService) {}
+
+  Nombre = signal('');
+  Apellidos = signal('');
+  Telefono = signal('');
+  Email = signal('');
+  Direccion = signal('');
+  Birthdate = signal('');
+
+  ngOnInit() {
+    if (!localStorage.getItem('Email')) {
+      console.log('No email found in localStorage. Redirecting to login page.');
+      this.router.navigate(['/login']);
+      return;
+    } else {
+      this.perfilService.getByEmail(localStorage.getItem('Email')?.toString() || '').subscribe(
+        (response: any) => {
+          // Reset form
+          this.Nombre.set(response.Nombre || '');
+          this.Apellidos.set(response.Apellidos || '');
+          this.Telefono.set(response.Telefono || '');
+          this.Email.set(response.Email || '');
+          this.Direccion.set(response.Direccion || '');
+          this.Birthdate.set(response.Birthdate || '');
+        },
+        (error: any) => {
+          console.error('Something wrong:', error);
+        },
+      );
+    }
+  }
+}
---
+++ src/app/components/perfil/perfil.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PerfilService {
+  private api = 'http://localhost:3000/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  getByEmail(Email: string) {
+    return this.http.get(`${this.api}/Email=${Email}`);
+  }
+}
