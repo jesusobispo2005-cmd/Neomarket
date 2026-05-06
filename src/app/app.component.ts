37ab0a406d1d8b79f53a5c3cec7650ed87c52a8a
Jesus Obispo <jesus.obispo@educa.madrid.org>
2026-05-06 12:56:54 +0200

    carrito arreglado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+
+  cartCount = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.cartService.cart$.subscribe(() => {
+      this.cartCount = this.cartService.getCount();
+    });
+  }
+}
---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface CartItem {
+  product: any;
+  cantidad: number;
+}
+
+@Injectable({ providedIn: 'root' })
+export class CartService {
+
+  private items: CartItem[] = [];
+  private cartSubject = new BehaviorSubject<CartItem[]>([]);
+
+  cart$ = this.cartSubject.asObservable();
+
+  addToCart(product: any, cantidad: number) {
+    const existing = this.items.find(i => i.product._id === product._id);
+    if (existing) {
+      existing.cantidad += cantidad;
+    } else {
+      this.items.push({ product, cantidad });
+    }
+    this.cartSubject.next([...this.items]);
+  }
+
+  removeFromCart(productId: string) {
+    this.items = this.items.filter(i => i.product._id !== productId);
+    this.cartSubject.next([...this.items]);
+  }
+
+  getTotal() {
+    return this.items.reduce((acc, i) => acc + i.product.Precio * i.cantidad, 0);
+  }
+
+  getCount() {
+    return this.items.reduce((acc, i) => acc + i.cantidad, 0);
+  }
+
+  clear() {
+    this.items = [];
+    this.cartSubject.next([]);
+  }
+}
