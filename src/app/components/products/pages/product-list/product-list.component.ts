30b5cd97c59ccdd089c1b1f53905876fd2b4b5ca
Jaume Puig Soriano <jaumepuigsoriano@gmail.com>
2026-05-13 13:27:26 +0200

    cambios css

--- src/app/components/products/pages/product-list/product-list.component.ts
+++ src/app/components/products/pages/product-list/product-list.component.ts
@@ -7,46 +7,41 @@ import { ProductFilterComponent } from '../../components/product-filter/product-
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [
-    CommonModule,
-    ProductCardComponent,
-    ProductFilterComponent
-  ],
+  imports: [CommonModule, ProductCardComponent, ProductFilterComponent],
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
-
   products: any[] = [];
   filteredProducts: any[] = [];
 
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
-  this.productService.getAll().subscribe({
-    next: (res: any) => {
-      this.products = res || [];
-      this.filteredProducts = res || [];
-    },
-    error: (err) => {
-      console.log('Error API:', err);
-      this.products = [];
-      this.filteredProducts = [];
-    }
-  });
-}
+
+    this.productService.getAll().subscribe({
+      next: (res: any) => {
+        this.products = res || [];
+        this.filteredProducts = res || [];
+      },
+      error: (err) => {
+        console.log('Error API:', err);
+        this.products = [];
+        this.filteredProducts = [];
+      },
+    });
+
+  }
 
   onFilterChange(filters: any) {
-    this.filteredProducts = this.products.filter(p => {
-
-      const matchSearch =
-        p.Nombre.toLowerCase().includes(filters.search.toLowerCase());
+    this.filteredProducts = this.products.filter((p) => {
+      const matchSearch = p.Nombre.toLowerCase().includes(filters.search.toLowerCase());
 
       const matchCategory =
-        !filters.category ||
-        p.Categoria?.toLowerCase() === filters.category.toLowerCase();
+        !filters.category || p.Categoria?.toLowerCase() === filters.category.toLowerCase();
 
       return matchSearch && matchCategory;
     });
   }
+
 }
