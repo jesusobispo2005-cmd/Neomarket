db239748eb83ff0542750a3e55ee608d27bd7f26
JaumePuig <jaumepuigsoriano@gmail.com>
2026-05-19 13:12:04 +0200

    borramos register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/products/pages/product-list/product-list.component';
 import { ProductDetailComponent } from './components/products/pages/product-detail/product-detail.component';
 import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
 import { CartComponent } from './components/cart/cart.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
 import { EditPerfilComponent } from './components/edit-perfil/edit-perfil.component';
